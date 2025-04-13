9390f869fbcaff2797176d9f2fd48f7469147a1c
Heshan Nanayakkara <kalaruheshan@gmail.com>
2025-04-13 21:08:48 +0530

    script modified

--- test/test.py
+++ test/test.py
@@ -1,14 +1,44 @@
 # Import dependencies
 from flask import Flask, jsonify
 from flask_cors import CORS
+import boto3
+import psycopg2
+import json
 
 # Create Flask application
 app = Flask(__name__)
 CORS(app)
 
+# DB connection configuration
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager', region_name='ap-southeast-1')
+secret = client.get_secret_value(SecretId='rs/admin/credentials')
+creds = json.loads(secret['SecretString'])
+
 @app.route("/test-app", methods=["GET"])
 def test_app():
     return jsonify({"message": "Testing App!"})
+
+@app.route("/test-db-conn", methods=["GET"])
+def test_db_conn():
+    try:
+        conn = psycopg2.connect(
+            host=creds['host'],
+            port=creds['port'],
+            user=creds['username'],
+            password=creds['password'],
+            dbname='mat-tdb'
+        )
+
+        cursor = conn.cursor()
+        cursor.execute('SELECT COUNT(*) FROM tdb.branch;')
+        count = cursor.fetchone()[0]
+        cursor.close()
+        conn.close()
+
+        return jsonify({"Count": count})
+    except Exception as e:
+        return jsonify({"Error connecting to Redshift": e})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
