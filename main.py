fe74e88e908755525492f3a80652e154b46bb5f4
Heshan Nanayakkara <kalaruheshan@gmail.com>
2025-04-14 19:46:21 +0530

    integration service completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from agent_service.microservice_logic import agent_bp
+from integration_service.microservice_logic import integration_bp
 
 # Create Flask application
 app = Flask(__name__)
@@ -8,6 +9,7 @@ CORS(app)
 
 # Route/Endpoint blueprints
 app.register_blueprint(agent_bp)
+app.register_blueprint(integration_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
