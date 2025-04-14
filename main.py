afdf65f285cf77f2b84348e00460201d065beb5d
Heshan Nanayakkara <kalaruheshan@gmail.com>
2025-04-14 23:44:28 +0530

    notification service completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from agent_service.microservice_logic import agent_bp
 from integration_service.microservice_logic import integration_bp
+from notification_service.microservice_logic import notification_bp
 
 # Create Flask application
 app = Flask(__name__)
@@ -10,6 +11,7 @@ CORS(app)
 # Route/Endpoint blueprints
 app.register_blueprint(agent_bp)
 app.register_blueprint(integration_bp)
+app.register_blueprint(notification_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
