87c69f80c332d499cbaba72d3e590da86237b237
Heshan Nanayakkara <kalaruheshan@gmail.com>
2025-04-15 11:33:46 +0530

    script modified

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import timezone
 from agent_service.microservice_logic import agent_bp
 from integration_service.microservice_logic import integration_bp
 from notification_service.microservice_logic import notification_bp
 from aggregator_service.microservice_logic import run_aggregator_service
-from apscheduler.schedulers.background import BackgroundScheduler
 
 # Create Flask application
 app = Flask(__name__)
@@ -17,7 +18,7 @@ app.register_blueprint(notification_bp)
 
 # Schedule the aggregator_service job daily at 7 AM
 scheduler = BackgroundScheduler()
-scheduler.add_job(run_aggregator_service, 'cron', hour=11, minute=40)
+scheduler.add_job(run_aggregator_service, 'cron', hour=11, minute=45, timezone=timezone('Asia/Colombo'))
 scheduler.start()
 
 if __name__ == '__main__':
